b42fe01da8234183764e49e76ccf6c434b534a44
holmqvist <fredrik.d.holmqvist@gmail.com>
2023-07-08 11:00:38 +0200

    5

--- src/sync.cr
+++ src/sync.cr
@@ -1,17 +1,21 @@
 require "./git"
 
-log, err = git_log()
-raise err unless err.empty?
+begin
+  log, err = git_log()
+  raise err unless err.empty?
 
-new_version = log.split("\n").size
+  new_version = log.split("\n").size
 
-err = git_add()
-raise err unless err.empty?
+  err = git_add()
+  raise err unless err.empty?
 
-err = git_commit(new_version)
-raise err unless err.empty?
+  err = git_commit(new_version)
+  raise err unless err.empty?
 
-err = git_push()
-raise err unless err.empty?
+  err = git_push()
+  raise err unless err.empty?
+rescue ex
+  STDERR.puts ex.message
+end
 
 puts "done"
