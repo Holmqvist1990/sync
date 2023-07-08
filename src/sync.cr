070dff4f29ef33cbdc325af33d555d2c4fc250ae
holmqvist <fredrik.d.holmqvist@gmail.com>
2023-07-08 10:59:16 +0200

    4

--- src/sync.cr
+++ src/sync.cr
@@ -1,34 +1,17 @@
 require "./git"
 
 log, err = git_log()
-unless err.empty?
-  STDERR.puts err
-  exit
-end
+raise err unless err.empty?
 
 new_version = log.split("\n").size
 
 err = git_add()
-unless err.empty?
-  STDERR.puts err
-end
+raise err unless err.empty?
 
 err = git_commit(new_version)
-unless err.empty?
-  STDERR.puts err
-end
+raise err unless err.empty?
 
 err = git_push()
-unless err.empty?
-  STDERR.puts err
-end
+raise err unless err.empty?
 
 puts "done"
-
-# result = Process.run("git", ["add", "."])
-# if result.success?
-#   puts "Git add succeeded"
-# else
-#   puts "Git add failed"
-#   puts "Exit code: #{result.exit_code}"
-# end
