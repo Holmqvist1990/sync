155bbecef68b13335142b4e38b89fd3d8b027427
holmqvist <fredrik.d.holmqvist@gmail.com>
2023-07-11 17:20:16 +0200

    sync auto-commit: #17

--- src/main.cr
+++ src/main.cr
@@ -3,19 +3,19 @@ require "./git"
 begin
 
   new_version, err = git_commit_number()
-  raise err unless err.empty?
+  raise "git_commit_number: #{err}"  unless err.empty?
 
   err = git_add()
-  raise err unless err.empty?
+  raise "git_add: #{err}"  unless err.empty?
 
   err = git_commit(new_version)
-  raise err unless err.empty?
+  raise "git_commit: #{err}"  unless err.empty?
 
   err = git_pull()
-  raise err unless err.empty?
+  raise "git_pull: #{err}"  unless err.empty?
 
   err = git_push()
-  raise err unless err.empty?
+  raise "git_push: #{err}" unless err.empty?
 rescue ex
   STDERR.puts "ERROR: #{ex.message}"
   exit(3)
