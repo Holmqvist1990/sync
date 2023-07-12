22dea344708f8c5f0ac3fc81f6c72aa8a840fa65
holmqvist <fredrik.d.holmqvist@gmail.com>
2023-07-12 18:24:24 +0200

    sync auto-commit: #22

--- src/main.cr
+++ src/main.cr
@@ -1,14 +1,20 @@
 require "../../git_helpers/src/git"
 
 begin
-  commit_number = Git.commit_number!()
-  Git.add_dot!()
+  commit_number = Git.commit_number!
+  Git.add_dot!
   Git.commit!("sync auto-commit: ##{commit_number}")
   Git.pull!(rebase: true)
-  Git.push!()
+  Git.push!
 rescue ex
-  STDERR.puts "ERROR: #{ex.message}"
-  exit(3)
+  message = ex.message
+  if message && message == "Everything up-to-date"
+    puts message
+    exit(0)
+  else
+    STDERR.puts "ERROR: #{ex.message}"
+    exit(3)
+  end
 end
 
-puts "done"
+puts "OK."
