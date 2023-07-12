cd2466f9fe24eb1cabc5bb601ffa20a77bde2ded
holmqvist <fredrik.d.holmqvist@gmail.com>
2023-07-12 17:29:43 +0200

    sync auto-commit: #20

--- src/main.cr
+++ src/main.cr
@@ -1,13 +1,33 @@
 require "../../git_helpers/src/git"
 
 begin
-  commit_number = Git.commit_number!()
-  Git.add_dot!()
-  Git.commit!("sync auto-commit: ##{commit_number}")
-  Git.pull!(rebase: true)
-  Git.push!()
+  commit_number = Git.commit_number!
 rescue ex
-  STDERR.puts "ERROR: #{ex.message}"
+  STDERR.puts "cm: #{ex.message}"
+  exit(3)
+end
+begin
+  Git.add_dot!
+rescue ex
+  STDERR.puts "ad: #{ex.message}"
+  exit(3)
+end
+begin
+  Git.commit!("sync auto-commit: ##{commit_number}")
+rescue ex
+  STDERR.puts "ct: #{ex.message}"
+  exit(3)
+end
+begin
+  Git.pull!(rebase: true)
+rescue ex
+  STDERR.puts "pl: #{ex.message}"
+  exit(3)
+end
+begin
+  Git.push!
+rescue ex
+  STDERR.puts "ps: #{ex.message}"
   exit(3)
 end
 
